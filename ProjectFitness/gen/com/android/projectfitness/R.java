2be66197bedf8c11798a40800197b158135044ba
Monica Rajendiran <mkr2ff@virginia.edu>
2012-11-01 09:42:53 -0400

    Android 2 Commit 3: Menu Added, Food Diary Added

--- ProjectFitness/gen/com/android/projectfitness/R.java
+++ ProjectFitness/gen/com/android/projectfitness/R.java
@@ -16,10 +16,13 @@ public final class R {
     }
     public static final class id {
         public static final int buttond=0x7f070003;
+        public static final int diary_menu_button=0x7f070008;
         public static final int editText1=0x7f070004;
         public static final int editText2=0x7f070005;
+        public static final int exercise_button=0x7f070009;
         public static final int foodList=0x7f070006;
-        public static final int menu_settings=0x7f070007;
+        public static final int list_button=0x7f070007;
+        public static final int menu_settings=0x7f07000a;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070001;
         public static final int textView3=0x7f070002;
@@ -27,6 +30,7 @@ public final class R {
     public static final class layout {
         public static final int activity_food_diary=0x7f030000;
         public static final int activity_food_list=0x7f030001;
+        public static final int activity_menu=0x7f030002;
     }
     public static final class menu {
         public static final int activity_calories=0x7f060000;
@@ -36,12 +40,17 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int calories_label=0x7f04000a;
         public static final int diary_button=0x7f04000b;
+        public static final int diary_menu_button=0x7f04000f;
         public static final int dining_hall_question=0x7f040003;
+        public static final int exercise_button=0x7f040010;
         public static final int food_diary_title=0x7f040008;
         public static final int hello_world=0x7f040006;
+        public static final int list_button=0x7f04000e;
         public static final int meal_label=0x7f040009;
         public static final int meals_and_calories=0x7f040005;
+        public static final int menu_message=0x7f04000d;
         public static final int menu_settings=0x7f040001;
+        public static final int menu_title=0x7f04000c;
         public static final int time_of_day_question=0x7f040004;
         public static final int title_activity_calories=0x7f040002;
         public static final int title_activity_food_list=0x7f040007;
---
+++ ProjectFitness/src/com/android/projectfitness/MenuActivity.java
@@ -0,0 +1,59 @@
+package com.android.projectfitness;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MenuActivity extends Activity {
+	
+	Button list = null;
+	Button diary = null;
+	Button exercise = null;
+
+	private OnClickListener listListener = new OnClickListener() {
+		public void onClick(View v) {
+
+			Intent myIntent = new Intent(v.getContext(), FoodListActivity.class);
+		       startActivityForResult(myIntent, 0);
+			
+		}
+	};
+	
+	private OnClickListener diaryListener = new OnClickListener() {
+		public void onClick(View v) {
+
+			Intent myIntent = new Intent(v.getContext(), FoodDiaryActivity.class);
+		       startActivityForResult(myIntent, 0);
+			
+		}
+	};
+	
+//	private OnClickListener exerciseListener = new OnClickListener() {
+//		public void onClick(View v) {
+//			
+//			Intent myIntent = new Intent(v.getContext(), ExerciseActivity.class);
+//		       startActivityForResult(myIntent, 0);
+//			
+//		}
+//	};
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        
+        list = (Button) findViewById(R.id.list_button);
+        diary = (Button) findViewById(R.id.diary_menu_button);
+        exercise = (Button) findViewById(R.id.exercise_button);
+        
+
+       list.setOnClickListener(listListener);
+       diary.setOnClickListener(diaryListener);
+       //exercise.setOnClickListener(exerciseListener);
+       
+    }
+}
